c25bc6db6dcd2705ac880c726335cf82d18c4269
Jaslyn Chen <jaslyn.p.chen9@gmail.com>
2024-07-30 10:51:19 -0400

    final question app project

---
+++ realQuestionApp/favoritecolor.swift
@@ -0,0 +1,18 @@
+//
+//  favoritecolor.swift
+//  realQuestionApp
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct favoritecolor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    favoritecolor()
+}
