696c0f9dd220057ffb0a498c8abe8dcf540292a5
Jaslyn Chen <jaslyn.p.chen9@gmail.com>
2024-07-30 09:52:51 -0400

    Initial Commit

---
+++ realQuestionApp/realQuestionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  realQuestionAppApp.swift
+//  realQuestionApp
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct realQuestionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
